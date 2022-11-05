0aed32762e6e787476d9fc25d9f6025214443685
PeytonWDYM <peytonvlogs34@gmail.com>
2022-11-05 14:26:07 -0500

    icecast-title parser script

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// parse icecast metadata
+function parseMetadata() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        // change mountpoint variable to match your mountpoint in icecast
+        mountpoint = "/yourmountpoint";
+        var response = JSON.parse(this.responseText);
+        var metadata = response[1].source[mountpoint].metadata.x_icy_title;
+        document.getElementById("metadata").innerHTML = "Currently Playing " + metadata;
+      }
+    };
+    // PUT YOUR ICECAST JSON URL HERE
+    // ONLY WORKS FOR ICECAST 2.5.0+
+    // icecast 2.5.0 and above: https://yourdomain:port/admin/publicstats.json
+    xhttp.open("GET", "https://example.com:8443/admin/publicstats.json", true);
+    xhttp.send();
+  }
+
+// parse icecast metadata every 1 second
+setInterval(parseMetadata, 1000);
