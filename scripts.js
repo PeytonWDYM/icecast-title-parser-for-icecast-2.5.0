a00aaad97ebdc606358faace443386ae98dd7d1a
PeytonWDYM <117424806+PeytonWDYM@users.noreply.github.com>
2022-11-06 18:14:47 -0600

    added some stuff
    Updated .metadata.x_icy_title to [displaytitle] added this for maximum flexibility 
    made it parse data every 10 seconds instead of one

--- scripts.js
+++ scripts.js
@@ -5,8 +5,9 @@ function parseMetadata() {
       if (this.readyState == 4 && this.status == 200) {
         // change mountpoint variable to match your mountpoint in icecast
         mountpoint = "/yourmountpoint";
+        displaytitle = "display-title";
         var response = JSON.parse(this.responseText);
-        var metadata = response[1].source[mountpoint].metadata.x_icy_title;
+        var metadata = response[1].source[mountpoint][displaytitle];
         document.getElementById("metadata").innerHTML = "Currently Playing " + metadata;
       }
     };
@@ -18,4 +19,4 @@ function parseMetadata() {
   }
 
 // parse icecast metadata every 1 second
-setInterval(parseMetadata, 1000);
+setInterval(parseMetadata, 10000);
