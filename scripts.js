ec795360bfd77a1befd2472ebdbd6d3eb219b245
Peyton <117424806+PeytonWDYM@users.noreply.github.com>
2024-11-30 02:55:49 -0600

    Use Fetch!!!!!!
    Yayyyaaayayataya

--- scripts.js
+++ scripts.js
@@ -1,22 +1,19 @@
-// parse icecast metadata
-function parseMetadata() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        // change mountpoint variable to match your mountpoint in icecast
-        mountpoint = "/yourmountpoint";
-        displaytitle = "display-title";
-        var response = JSON.parse(this.responseText);
-        var metadata = response[1].source[mountpoint][displaytitle];
-        document.getElementById("metadata").innerHTML = "Currently Playing " + metadata;
-      }
-    };
-    // PUT YOUR ICECAST JSON URL HERE
-    // ONLY WORKS FOR ICECAST 2.5.0+
-    // icecast 2.5.0 and above: https://yourdomain:port/admin/publicstats.json
-    xhttp.open("GET", "https://example.com:8443/admin/publicstats.json", true);
-    xhttp.send();
-  }
+let interval = 10000
+let mountpoint = "/yourmountpoint";
+let icecastLink = "https://yourdomain:port/admin/publicstats.json";
 
-// parse icecast metadata every 10 seconds
-setInterval(parseMetadata, 10000);
+
+function parseMetadata() {
+    fetch('https://example.com:8443/admin/publicstats.json')
+    .then((response) => response.json())
+    .then((data) => {
+        var metadata = data.source[mountpoint]["display-title"];
+        document.getElementById("metadata").innerHTML = "Currently Playing " + metadata;
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+
+setInterval(parseMetadata, interval);
